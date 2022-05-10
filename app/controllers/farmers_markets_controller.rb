c29ba4c7e2cb35c21017e3aeecb85769238dd22e
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-10 17:23:41 -0400

    Create route and controller for farmers markets

---
+++ app/controllers/farmers_markets_controller.rb
@@ -0,0 +1,2 @@
+class FarmersMarketsController < ApplicationController
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/farmers_markets', to: 'farmers_markets#index'
 end
