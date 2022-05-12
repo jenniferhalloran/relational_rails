62aa340430336189303da53fdc74f23aafe3edd6
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-12 15:56:28 -0400

    Add validations and tests for stand model

--- app/models/stand.rb
+++ app/models/stand.rb
@@ -1,4 +1,7 @@
 class Stand < ApplicationRecord
   belongs_to :farmers_market
   #this allows us to call a method to see the farmers market (.farmers_market)
+  validates_presence_of :name
+  validates_presence_of :review_rating
+  validates_inclusion_of :open, :in => [true, false]
 end
