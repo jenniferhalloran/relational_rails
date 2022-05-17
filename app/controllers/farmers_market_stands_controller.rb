67e6d9693cfeb7075d9fa43421f5a5810ed85a16
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-16 20:59:59 -0400

    Update model, controller, and view to filter by review rating

--- app/controllers/farmers_market_stands_controller.rb
+++ app/controllers/farmers_market_stands_controller.rb
@@ -1,10 +1,13 @@
 class FarmersMarketStandsController < ApplicationController
   def index
     @farmers_market = FarmersMarket.find(params[:id])
-    @stands = @farmers_market.stands
     if params[:sort] == "name"
       @stands = @farmers_market.stands.order(:name)
+    elsif params[:rating_over]
+      @stands = @farmers_market.stands.rating_over(params[:rating_over])
+    else
+      @stands = @farmers_market.stands
     end
-    
+
   end
 end
