143af5199990f24d426767aa2ec162ac77f0c768
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-17 14:09:33 -0400

    Update class methods for farmers market model

--- spec/models/farmers_market_spec.rb
+++ spec/models/farmers_market_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe FarmersMarket, type: :model do
   describe 'relationships' do
     it {should have_many :stands}
   end
+
+  describe 'class methods' do
+    before (:each) do
+      @slo =FarmersMarket.create!(name: "SLO Farmers Market", city: "San Luis Obispo", open: true, num_stands: 15)
+      @hillcrest = FarmersMarket.create!(name: "Hillcrest Farmers Market", city: "San Diego", open: true, num_stands: 46)
+      @ferry_plaza = FarmersMarket.create!(name: "Ferry Plaza Farmers Market", city: "San Francisco", open: false , num_stands: 25)
+      @greenmarket = FarmersMarket.create!(name: "Union Square Greenmarket", city: "New York", open: true , num_stands: 87)
+    end
+    it "orders by creation time" do
+      expect(FarmersMarket.ordered_by_creation).to eq([@greenmarket, @ferry_plaza, @hillcrest, @slo])
+    end
+  end
+
 end
